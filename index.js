32aeaa2e7d6661df497f4773a88b7b4d21ee2709
laushinka <laurie.malau@gmail.com>
2016-07-25 15:56:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+function fatBastard(meal){
+  function whatsForDinner(){
+    if (!meal) {
+      console.log("My belly is empty.")
+    } else if (meal === "Mini-Me") {
+      console.log("The wee man is in my belly!")
+    } else {
+      console.log(`I'm eatin a bit of ${meal}! Burp.`)
+    }
+  }
+  function digest() {
+    meal = undefined;
+  }
+  // return {
+    x = whatsForDinner,
+    y = digest
+  // };
+}
+
+const {whatsForDinner, digest} = fatBastard('ribeye');
+whatsForDinner();
+digest();
+whatsForDinner();
+
+const tummy = fatBastard("Kobe beef");
+tummy();
+
+function raspyDoorGuy(){
+  const password = 'yarr';
+
+  function givePassword(givenPassword) {
+    if (givenPassword === password) {
+      console.log('Ye may enter.')
+    } else {
+      console.log('Begone, landlubber!')
+    }
+  }
+  return {
+    givePassword
+  };
+}
